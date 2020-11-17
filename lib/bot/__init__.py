66851b4ac1773b0ef498d72d70995275e1775431
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-11-17 20:09:37 +0530

    :zap: Adding command support

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -2,39 +2,67 @@ from discord.ext.commands import Bot as BotBase
 from discord.ext.commands import Context
 from discord.ext.commands import CommandNotFound
 from discord import Embed, File
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from glob import glob
 from datetime import datetime 
 from asyncio import sleep
 
 PREFIX = "!"
 OWNER_IDS = [572353145963806721]
+COMMANDS = [path.split("\\")[-1][:-3] for path in glob("./lib/commands/*.py")]
+
+class Ready(object):
+    def __init__(self):
+        for command in COMMANDS:
+            setattr(self, command, False)
+    def ready_up(self, command):
+        setattr(self, command, True)
+        print(f"{command} command is ready")
+
+    def all_ready(self):
+        return all([getattr(self, command) for command in COMMANDS])
 
 class Bot(BotBase):
     def __init__(self):
         self.PREFIX = PREFIX
         self.ready = False
+        self.command_ready = Ready()
         self.guild = None
-        #Super class Bot base
+        self.scheduler = AsyncIOScheduler()
+
         super().__init__(command_prefix=PREFIX, OWNER_ID=OWNER_IDS)
 
-    # Run Bot
+    def setup(self):
+        print("setup Run")
+        for command in COMMANDS:
+            self.load_extension(f"lib.commands.{command}") ## Checkn and Debug
+            print(f"{command} cog Loaded")
+
     def run(self, version):
         self.VERSION = version
+        print("running setup")
+        self.setup()
 
-        with open("./lib/bot/token","r") as tf:
+        with open("./lib/bot/token", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
 
         print("Running Gift Bot...")
         super().run(self.TOKEN, reconnect=True)
 
-    #On Connection
+    async def process_commands(self,message):
+        ctx = await self.get_context(message, cls=Context)
+        if ctx.command is not None and ctx.guild is not None:
+            if self.ready:
+                await self.invoke(ctx)
+            else:
+                await self.send("Wait for Oneechan to be ready!! ")
+    
     async def on_connect(self):
         print("Ara Ara!")
 
-    #On disconnection
     async def on_disconnect(self):
         print("Ara Ara Sionara!")
 
-    #On Error
     async def on_error(self, err, *args, **kwargs):
         if err == 'on_command_error':
             await args[0].send("Somthing went wrong.")
@@ -42,19 +70,31 @@ class Bot(BotBase):
         await channel.send("An error Occurrred")
         raise 
 
-    #On Bot Ready
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+        # elif hasattr(exc, "original"):
+        #     raise exc.original
+        else:
+            raise exc
+
+
     async def on_ready(self):
         if not self.ready:
             self.guild = self.get_guild(710051662563115049)
             self.stdout = self.get_channel(710051662563115052)
-
+            self.scheduler.start()
+            while not self.command_ready.all_ready():
+                print("waiting......")
+                await sleep(0.5)
             self.ready = True
-            print("Gift Bot Ready")
-            await self.stdout.send("Now Online!!")
-
+            print("Gift Bot ready")
+            await self.stdout.send("Now Online")
         else:
-            print("Gift Bot Reconnected")
+            print("Gift Bot reconnected")
 
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.process_commands(message)
 
-#Initialize Bot
 bot = Bot()
---
+++ lib/commands/commands.py
@@ -0,0 +1,38 @@
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+from discord import Member
+from typing import Optional
+from random import choice, randint
+
+class Command(Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @command(name="hello", aliases=["hola", "h"], hidden=True)
+    async def say_hello(self, ctx):
+        await ctx.send(f"{choice(('Konnichiva', 'Ara Ara', 'Okaeri', 'Yahallo'))} {ctx.author.mention}!")
+        pass
+
+    # @command(name="roll", aliases=["Roll", "dice", "Dice"])
+    # async def roll_dice(self, ctx, die_str: str):
+    #     dice,value = (int(term) for term in die_str.split("d"))
+    #     rolls = [randint(1,value) for i in range(dice)]
+    #     await ctx.send(" + ".join([str(roll) for roll in rolls]) + f" = {sum(rolls)}")
+        
+    # @command(name="slap", aliases=["hit", "fuck"])
+    # async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "existing"):
+    #     await ctx.send(f"{ctx.author.display_name} slapped {member.mention} for {reason}")
+
+    # @command(name="echo", aliases=["say", "shout"])
+    # async def echo_message(self, ctx, *, message):
+    #     await ctx.message.delete()
+    #     await ctx.send(message)
+
+    @Cog.listener()
+    async def on_ready(self):
+        # await self.bot.stdout.send("Command Cog ready")
+        if not self.bot.ready:
+            self.bot.command_ready.ready_up("commands")
+
+def setup(bot):
+    bot.add_cog(Command(bot))
